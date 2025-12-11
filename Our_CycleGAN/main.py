323ff366edc8804486bd861bcfbabddab29157cc
Nah man, i lose. <selaruang@gmail.com>
2025-12-11 21:54:04 +0700

    Update evaluation evaluate_quantitative

--- Our_CycleGAN/main.py
+++ Our_CycleGAN/main.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
 from model_training import CustomDataset, Trainer
 from CycleGAN_arch import CycleGAN
-from model_evaluation import evaluate_quantitative
+from model_evaluation import evaluate_quantitative, evaluate_qualitative
+from tqdm import tqdm
+import json
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 CHECKPOINT_FILE = "cyclegan_checkpoint.pth"
@@ -26,30 +28,64 @@ def data_preprocessing():
     
 
 def model_evaluation(test_loader, device, model_path, n_sample=7):
+    def denormalize_image_tensor(image_tensor):
+        """
+        Denormalizes the tensor output (C, H, W) from [-1, 1] to [0, 1] and plots it.
+        """
+        image_tensor = image_tensor * 0.5 + 0.5
+        image_tensor = image_tensor.clamp(0.0, 1.0)
+        return image_tensor
+    
+    
     model = CycleGAN(device, only_G_A2B=True)
     model.model_init()
     model.load_model(model_path = model_path)
     model.eval()
     
+
+    
     random_idx = np.random.choice(len(test_loader), n_sample, replace=False).tolist()
     compare_image = torch.zeros((len(random_idx), 3, 3, 256, 256))
     
-    print("Start inference...")
+    metrics = []
+    
+    print("Start inference and evaluation...")
     iter_round = 0
-    for i, (low_light_img, normal_light_img) in enumerate(test_loader):
+    progress_bar = tqdm(test_loader)
+    for i, (low_light_img, normal_light_img) in enumerate(progress_bar):
+        low_light_img_device = low_light_img.to(device)  
+        generated_img = model.G_A2B(low_light_img_device)
+        generated_img = generated_img.detach().cpu().clone()
+        
+        # Denormalize
+        low_light_img = denormalize_image_tensor(low_light_img)
+        normal_light_img = denormalize_image_tensor(normal_light_img)
+        generated_img = denormalize_image_tensor(generated_img)
+        
+        #--------------------------
+        # Quantitative Evaluation
+        #--------------------------
+        metrics.append(evaluate_quantitative(generated_img, normal_light_img, device))
+        
         if i in random_idx:
-            random_idx.remove(i)
-            low_light_img_device = low_light_img.to(device)  
-            predicted_img = model.G_A2B(low_light_img_device)
-            predicted_img = predicted_img.detach().cpu().clone()
-            compare_image[iter_round] = torch.cat([low_light_img, normal_light_img, predicted_img], dim=0)
-            if len(random_idx) == 0:
-                break
+            compare_image[iter_round] = torch.cat([low_light_img, normal_light_img, generated_img], dim=0)
             iter_round += 1
             
-    print("Start evaluation...")
-    evaluate_quantitative(compare_image)
-    print("End evaluation...")
+    metrics = pd.DataFrame(metrics)
+    metrics.to_csv("Our_CycleGAN/metrics.csv", index=False)
+    metric_dict = metrics.mean().to_dict()
+    with open("Our_CycleGAN/metric_dict.json", "w") as f:
+        json.dump(metric_dict, f)
+    
+            
+    #--------------------------
+    # Qualitative Evaluation
+    #--------------------------
+    evaluate_qualitative(compare_image)
+    print("Evaluation finished.")
+    
+
+    
 
 def model_training(df_train, device):
     trainer = Trainer(device = device, model_name=MODEL_NAME, model=CycleGAN)
--- Our_CycleGAN/model_evaluation.py
+++ Our_CycleGAN/model_evaluation.py
@@ -3,34 +3,105 @@ import torch
 from CycleGAN_arch import CycleGAN
 import matplotlib.pyplot as plt
 
+from skimage.metrics import peak_signal_noise_ratio as psnr_ski
+from skimage.metrics import structural_similarity as ssim_ski
+import pyiqa
+
+import sys
+import os
+from contextlib import contextmanager
+
+
+@contextmanager
+def suppress_stdout():
+    """Context manager to temporarily redirect stdout to /dev/null."""
+    with open(os.devnull, 'w') as null_file:
+        # Save the original stdout
+        original_stdout = sys.stdout
+        # Redirect stdout to the null file
+        sys.stdout = null_file
+        try:
+            # Yield control back to the 'with' block
+            yield
+        finally:
+            # Restore the original stdout
+            sys.stdout = original_stdout
+
+
 def save_fig_png(fig, save_path):
     fig.savefig(save_path, dpi=300, bbox_inches='tight')
 
-def evaluate_qualitative():
-    pass
 
-def evaluate_quantitative(compare_image):
+def evaluate_quantitative(generated_image_tensor, real_image_tensor, device):
+    # Convert to numpy (H, W, C)
+    generated_image_numpy = generated_image_tensor.squeeze().permute(1, 2, 0).numpy()
+    real_image_numpy = real_image_tensor.squeeze().permute(1, 2, 0).numpy()
+    
+    generated_image_tensor = generated_image_tensor.to(device)
+    real_image_tensor = real_image_tensor.to(device)    
+    
+    def calculate_psnr(generated_image_numpy, real_image_numpy):
+        """
+        Calculate the PSNR, SSIM, and LPIPS metrics.
+        """
 
-    def tensor_to_rgb(image_tensor):
+        psnr_score = psnr_ski(real_image_numpy, generated_image_numpy, data_range=1.0)
+    
+        return psnr_score
+    
+    def calculate_ssim(generated_image_numpy, real_image_numpy):
         """
-        Denormalizes the tensor output (C, H, W) from [-1, 1] to [0, 1] and plots it.
+        Calculate the SSIM metric.
         """
-        image_tensor = image_tensor.squeeze(0)
-        image_tensor = image_tensor * 0.5 + 0.5
-        image_tensor = image_tensor.clamp(0.0, 1.0)
-        return image_tensor
+        ssim_score = ssim_ski(real_image_numpy, generated_image_numpy, data_range=1.0, multichannel=True, channel_axis=-1)
+        
+        return ssim_score
+    
+    def calculate_lpips(generated_image_tensor, real_image_tensor):
+        """
+        Calculate the LPIPS metric.
+        """
+        lpips_score = None
+        with suppress_stdout():
+            lpips_metric = pyiqa.create_metric('lpips', device=device)
+            
+            with torch.no_grad():
+                lpips_score = lpips_metric(generated_image_tensor, real_image_tensor).item() # Note the order: (dist, ref)
+            
+        return lpips_score
+    
+    def calculate_niqe(generated_image_tensor):
+        """
+        Calculate the NIQE metric.
+        """
+        niqe_metric = pyiqa.create_metric('niqe', device=device)
+        with torch.no_grad():
+            niqe_score = niqe_metric(generated_image_tensor).item()
+
+        return niqe_score
+    
+    psnr_score = calculate_psnr(generated_image_numpy, real_image_numpy)
+    ssim_score = calculate_ssim(generated_image_numpy, real_image_numpy)
+    lpips_score = calculate_lpips(generated_image_tensor, real_image_tensor)
+    niqe_score = calculate_niqe(generated_image_tensor)
+    
+    metrics = {'PSNR': psnr_score, 'SSIM': ssim_score, 'LPIPS': lpips_score, 'NIQE': niqe_score}
+    
+    return metrics
+
+def evaluate_qualitative(compare_image):
         
     def compare_plot(compare_image, titles="Compare Images (Low Light, Normal Light, Generated)"):
-        # compare_image: (Set, Type, Channels, Height, Width)
-        # compare_image: (n_Set, 3, 3, 286, 286)
+        """
+        Plot the compare image (Low Light, Normal Light, Generated).
+        compare_image: (Set, Type, Height, Width, Channels)
+        compare_image: (n_Set, 3, 286, 286, 3)
+        """
         
         n_coloumn = compare_image.size(0)
         n_row = compare_image.size(1)
         
-        for i in range(n_coloumn):
-            for j in range(n_row):
-                compare_image[i, j] = tensor_to_rgb(compare_image[i, j])
-                
+        compare_image = compare_image.squeeze()
         compare_image = compare_image.permute(0, 1, 3, 4, 2) # (Set, Type, Height, Width, Channels)
                 
         title_types = ["Low Light", "Normal Light", "Generated"]
@@ -46,7 +117,6 @@ def evaluate_quantitative(compare_image):
                     
                 axes[j, i].imshow(compare_image[i, j])
         fig.suptitle(titles, fontsize=16)
-        # fig.tight_layout()
                 
         return fig
     
